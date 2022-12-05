7b8d3174b7626ddba1fadb913b154f5e6f6ad81c
rayxie11 <rayxie11@stanford.edu>
2022-12-05 12:40:47 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -178,8 +178,8 @@ def video_data_gen_full(feature_extractor, img_height = 720, img_width = 1080, f
     # Get minimum video size and video slice indices
     vid_min_size, genre_idx_dict, _ = get_min_segment_val()
 
-    print(genre_idx_dict)
-    print(vid_min_size)
+    #print(genre_idx_dict)
+    #print(vid_min_size)
     return
 
     # Check if given frame is larger than minimum video length
